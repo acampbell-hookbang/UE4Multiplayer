49702194ec4c64b7fe83818a2aade77a6c4c8180
A. T. Campbell, III <acampbell@hookbang.com>
2018-10-30 11:39:50 -0500

    Lectures 10-11: Create FPSObjective class amd setup ActorComponents

---
+++ Section2_Stealth/Source/FPSGame/Private/FPSObjective.cpp
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "FPSObjective.h"
+#include "Components/SphereComponent.h"
+
+// Sets default values
+AFPSObjective::AFPSObjective()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComp"));
+	RootComponent = MeshComp;
+	SphereComp = CreateDefaultSubobject<USphereComponent>(TEXT("SphereComp"));
+	SphereComp->SetupAttachment(MeshComp);
+}
+
+// Called when the game starts or when spawned
+void AFPSObjective::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AFPSObjective::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
