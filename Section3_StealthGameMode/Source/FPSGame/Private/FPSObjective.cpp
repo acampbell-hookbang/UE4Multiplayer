868f88bc32d9609af97c659c79a009bca630b022
A. T. Campbell, III <acampbell@hookbang.com>
2018-10-30 17:22:03 -0500

    Carry over Stealth game from Sections 2 to 3.

---
+++ Section3_StealthGameMode/Source/FPSGame/Private/FPSObjective.cpp
@@ -0,0 +1,48 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "FPSObjective.h"
+#include "Components/SphereComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "FPSCharacter.h"
+
+// Sets default values
+AFPSObjective::AFPSObjective()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComp"));
+	MeshComp->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	RootComponent = MeshComp;
+	SphereComp = CreateDefaultSubobject<USphereComponent>(TEXT("SphereComp"));
+	SphereComp->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	SphereComp->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Ignore);
+	SphereComp->SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn, ECollisionResponse::ECR_Overlap);
+	SphereComp->SetupAttachment(MeshComp);
+}
+
+// Called when the game starts or when spawned
+void AFPSObjective::BeginPlay()
+{
+	Super::BeginPlay();
+	PlayEffects();
+}
+
+void AFPSObjective::PlayEffects()
+{
+	UGameplayStatics::SpawnEmitterAtLocation(this, PickupFX, GetActorLocation());
+}
+
+void AFPSObjective::NotifyActorBeginOverlap(AActor * OtherActor)
+{
+	Super::NotifyActorBeginOverlap(OtherActor);
+	PlayEffects();
+	AFPSCharacter* myCharacter = Cast<AFPSCharacter>(OtherActor);
+	if (myCharacter != nullptr)
+	{
+		myCharacter->bIsCarryingObjective = true;
+		Destroy();
+	}
+}
+
