299c8f979940c755ae170d588d7b02fdbceeb24c
A. T. Campbell, III <acampbell@hookbang.com>
2018-10-31 10:53:43 -0500

    Lecture 17: Implement Extraction Zone

---
+++ Section3_StealthGameMode/Source/FPSGame/Private/FPSExtractionZone.cpp
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "FPSExtractionZone.h"
+#include "FPSCharacter.h"
+#include "Components/BoxComponent.h"
+
+// Sets default values
+AFPSExtractionZone::AFPSExtractionZone()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	OverlapComp = CreateDefaultSubobject<UBoxComponent>(TEXT("OverlapComp"));
+	OverlapComp->SetBoxExtent(FVector(200.f));
+	OverlapComp->SetHiddenInGame(false);
+	OverlapComp->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	OverlapComp->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Ignore);
+	OverlapComp->SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn, ECollisionResponse::ECR_Overlap);
+	RootComponent = OverlapComp;
+}
+
+void AFPSExtractionZone::NotifyActorBeginOverlap(AActor * OtherActor)
+{
+	Super::NotifyActorBeginOverlap(OtherActor);
+
+	UE_LOG(LogTemp, Warning, TEXT("AFPSExtractionZone: Overlap with Extraction Zone"));
+	AFPSCharacter* myCharacter = Cast<AFPSCharacter>(OtherActor);
+	if (myCharacter != nullptr)
+	{
+	}
+}
